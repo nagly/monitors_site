cb7f95bcf28cbc13d3136843f3540cf32d01909a
nagly <nagly.piotrek@gmail.com>
2015-11-25 11:13:58 +0100

    Add Hyip model

--- hyips/models.py
+++ hyips/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
- class Hyip(models.Model):
+class Hyip(models.Model):
  	title = models.CharField(max_length=100)
  	url = models.URLField()
  	created = models.DateTimeField(auto_now_add=True, auto_now=False)

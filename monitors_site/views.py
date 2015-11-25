d9a56f52eb5b93854579ad2f7161b9301220dea9
nagly <nagly.piotrek@gmail.com>
2015-11-25 18:26:18 +0100

    Basic search

--- monitors_site/views.py
+++ monitors_site/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from monitors.models import Monitor
 from hyips_info.models import Hyips_info
 from hyips.models import Hyip
 def home(request):
 	context = {}
 	template = 'home.html'
+	# if request.method == 'POST':
+	# 	req = request.POST
+	# 	program_url = req.get('program_url', False)
+	# 	hyip = Hyip.objects.filter(url__icontains='%s' %program_url)
+	# 	context = {'hyip': hyip}
 	return render(request, template, context)
 
 def show_hyip(request, hyip_id):
@@ -17,3 +23,13 @@ def show_hyip(request, hyip_id):
 	context = {'monitors':monitors, 'hyip':hyip}
 	template = 'hyip_page.html'
 	return render(request, template, context)
+
+def search(request):
+	if request.method == 'POST':
+		req = request.POST
+		program_url = req.get('program_url', False)
+		hyip = Hyip.objects.filter(url__icontains='%s' %program_url)
+		if hyip.count() == 1:
+			return HttpResponseRedirect('/details/%s/' %hyip[0].id)
+		else:
+			return HttpResponseRedirect('/')

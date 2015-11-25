6b1eeaa639f89755acfc0e54243a8af1a9d69c36
nagly <nagly.piotrek@gmail.com>
2015-11-25 11:04:31 +0100

    Add Hyip model

--- hyips/admin.py
+++ hyips/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from hyips.models import Hyip
 # Register your models here.
+
+class HyipAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Hyip
+	list_display = ('id','title','url','created')
+admin.site.register(Hyip, HyipAdmin)

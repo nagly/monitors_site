9ce521115b22f4dcd204c6ba1f450271d61cbf46
nagly <nagly.piotrek@gmail.com>
2015-11-25 11:35:21 +0100

    Add Monitor model

--- monitors/admin.py
+++ monitors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from monitors.models import Monitor
 # Register your models here.
+class MonitorAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Monitor
+	list_display = ('url','url_hyip_details','url_card')
+admin.site.register(Monitor, MonitorAdmin)
--- monitors/models.py
+++ monitors/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Monitor(models.Model):
+	url = models.URLField()
+	url_hyip_details = models.CharField(max_length=100)
+	url_card = models.CharField(max_length=100)

20edf6ce1c7a73e114b6fcc5e636a3d5d37a93d7
nagly <nagly.piotrek@gmail.com>
2015-11-25 12:13:54 +0100

    Fix for model display with smart unicode

--- hyips/models.py
+++ hyips/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from django.utils.encoding import smart_unicode
 # Create your models here.
 class Hyip(models.Model):
- 	title = models.CharField(max_length=100)
- 	url = models.URLField()
- 	created = models.DateTimeField(auto_now_add=True, auto_now=False)
- 		
+	title = models.CharField(max_length=100)
+	url = models.URLField()
+	created = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+	def __unicode__(self):
+		return smart_unicode(self.title)
--- monitors/models.py
+++ monitors/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from django.utils.encoding import smart_unicode
 # Create your models here.
 class Monitor(models.Model):
 	url = models.URLField()
 	url_hyip_details = models.CharField(max_length=100)
 	url_card = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return smart_unicode(self.url)

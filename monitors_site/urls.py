5b573157daceebe66df8f809b700589f9925ac91
nagly <nagly.piotrek@gmail.com>
2015-11-25 17:26:30 +0100

    Add programs page

--- monitors_site/urls.py
+++ monitors_site/urls.py
@@ -20,4 +20,5 @@ from monitors_site import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home, name='home'),
+    url(r'^details/(?P<hyip_id>.+)/', views.show_hyip),
 ]
--- monitors_site/views.py
+++ monitors_site/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from monitors.models import Monitor
+from hyips_info.models import Hyips_info
+from hyips.models import Hyip
 def home(request):
 	context = {}
 	template = 'home.html'
+	return render(request, template, context)
+
+def show_hyip(request, hyip_id):
+	hyip = Hyip.objects.get(id=hyip_id)
+	monitors = Monitor.objects.all()
+	for monitor in monitors:
+		lid = Hyips_info.objects.get(hyip_id=hyip_id, monitor_id=monitor.id)
+		monitor.url_hyip_details = monitor.url_hyip_details.replace('{{listing_id}}', '%s' %lid.listing_id)
+		monitor.url_card = monitor.url_card.replace('{{listing_id}}', '%s' %lid.listing_id)
+	context = {'monitors':monitors, 'hyip':hyip}
+	template = 'hyip_page.html'
 	return render(request, template, context)

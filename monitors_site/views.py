4276074c7ea62a1e55ba945fe27d026f83fb4002
nagly <nagly.piotrek@gmail.com>
2015-11-25 20:06:31 +0100

    Fix for show program

--- monitors_site/views.py
+++ monitors_site/views.py
@@ -17,11 +17,13 @@ def home(request):
 def show_hyip(request, hyip_id):
 	hyip = Hyip.objects.get(id=hyip_id)
 	monitors = Monitor.objects.all()
-	for monitor in monitors:
-		lid = Hyips_info.objects.get(hyip_id=hyip_id, monitor_id=monitor.id)
-		monitor.url_hyip_details = monitor.url_hyip_details.replace('{{listing_id}}', '%s' %lid.listing_id)
-		monitor.url_card = monitor.url_card.replace('{{listing_id}}', '%s' %lid.listing_id)
-	context = {'monitors':monitors, 'hyip':hyip}
+	monitors_for_hyip = Hyips_info.objects.filter(hyip_id=hyip_id) #liczba stron monitorujacych konkretnego hyipa
+	if monitors_for_hyip.count() > 0:
+		for monitor in monitors:
+			lid = Hyips_info.objects.get(hyip_id=hyip_id, monitor_id=monitor.id)
+			monitor.url_hyip_details = monitor.url_hyip_details.replace('{{listing_id}}', '%s' %lid.listing_id)
+			monitor.url_card = monitor.url_card.replace('{{listing_id}}', '%s' %lid.listing_id)
+	context = {'monitors':monitors, 'hyip':hyip, 'monitors_for_hyip':monitors_for_hyip.count()}
 	template = 'hyip_page.html'
 	return render(request, template, context)
 

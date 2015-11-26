b6a1c82c904df932ef653d969c4848ba19496138
nagly <nagly.piotrek@gmail.com>
2015-11-27 00:18:03 +0100

    Add context_processors

--- monitors_site/settings.py
+++ monitors_site/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'monitors_site.context_processors.current_url',
             ],
         },
     },
